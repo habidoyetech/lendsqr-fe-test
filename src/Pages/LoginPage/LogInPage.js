19c890deee4be3b32afc120aa3d5894e27c82a68
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-10-15 14:09:20 +0100

    Add an element to show button and use usestate to display the password

--- src/Pages/LoginPage/LogInPage.js
+++ src/Pages/LoginPage/LogInPage.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogoWord from '../../images/lendsqr.svg'
 import Logo from '../../images/Union.svg'
 import SideImage from '../../images/pablo-sign-in 1.svg'
 import './loginpage.scss'
 
 const LogInPage = () => {
+
+  const [type, setType] = useState('password')
+
+  const changeInputType =  () => {
+    if (type === 'password') {
+      setType('text')
+    } else {
+      setType('password')
+    }
+  }
+
   return (
     <div className='log-in-page'>
       <div className='log-in container'>
@@ -25,12 +36,13 @@ const LogInPage = () => {
             <p>Enter Login details</p>
             <form action="#">
               <div className='input-block email-container'>
-                <input type="email" required/>
+                <input type="email" id='email' required/>
                 <label className='placeholder' htmlFor="email">Email</label>
               </div>
               <div className='input-block password-container'>
-                <input type="password" />
+                <input type={type} id='password' required/>
                 <label className='placeholder' htmlFor="password">Password</label>
+                <i onClick={changeInputType} id='togglePassword'>show</i>
               </div>
               <div className='forget-password-link-container'>
                 <a className='forget-password-link' href="#">Forgot Password?</a>
